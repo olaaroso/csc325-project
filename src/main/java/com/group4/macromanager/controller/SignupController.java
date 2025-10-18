b6e4ce7ae4aefe6fa8de53c553a5b5dbd34540e3
Josh Iehle <iehlejosh@gmail.com>
2025-10-18 17:43:01 -0400

    sprint1: create initial design and styles for dashboard page and sidebar

--- src/main/java/com/group4/macromanager/controller/SignupController.java
+++ src/main/java/com/group4/macromanager/controller/SignupController.java
@@ -1,5 +1,6 @@
 package com.group4.macromanager.controller;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Hyperlink;
 import java.io.IOException;
@@ -22,5 +23,13 @@ public class SignupController {
                 ex.printStackTrace();
             }
         });
+    }
+
+    @FXML
+    public void handleSignupButtonClick(ActionEvent event) throws IOException {
+        // This handler will process the signup and registration logic
+
+        // FOR NOW: this button navigates directly to the login page
+        PageNavigationManager.switchTo("loginPage.fxml");
     }
 }
