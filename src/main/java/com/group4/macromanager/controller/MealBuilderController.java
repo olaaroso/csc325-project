1e257a2c9aa37228a9a6debc226ec67877029b99
Josh Iehle <iehlejosh@gmail.com>
2025-10-31 14:51:03 -0400

    sprint1/sidebar-component: turn sidebar into a reusable component and set the proper active page in each page controller

--- src/main/java/com/group4/macromanager/controller/MealBuilderController.java
+++ src/main/java/com/group4/macromanager/controller/MealBuilderController.java
@@ -1,4 +1,17 @@
 package com.group4.macromanager.controller;
 
+import javafx.fxml.FXML;
+
 public class MealBuilderController {
+
+    // FXML elements
+    @FXML
+    private SidebarController sidebarIncludeController;
+
+    // Initialize function
+    @FXML
+    public void initialize() {
+        // Highlight current page in the sidebar
+        sidebarIncludeController.setActivePage("mealBuilder");
+    }
 }
